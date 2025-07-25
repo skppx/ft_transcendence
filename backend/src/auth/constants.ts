7d0dc6719b507ac33a86aa6f50345d9f01912901
phabets <phabets@student.42.fr>
2025-07-25 16:35:15 +0200

    fixed 42api updtate

--- backend/src/auth/constants.ts
+++ backend/src/auth/constants.ts
@@ -4,7 +4,7 @@ const CONST_URL = 'https://api.intra.42.fr/oauth/token';
 const CONST_FRONTEND_URL = 'http://localhost:3000';
 const CONST_INFO_URL = 'https://api.intra.42.fr/oauth/token/info';
 const CONST_CALLBACK_URL =
-  'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-596cda66f0747475f74108ccc47067de3786961428935f77fc53f37507af856b&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&response_type=code';
+  'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-f52da636dd6edc444bb9854b664906654754e74cb7cc4fe1d448ee6930136d84&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&response_type=code';
 const CONST_SALT = 10;
 const CONST_LOCAL_LOGIN = 'http://localhost:3000/auth/login';
 
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 const CONST_AUTHORIZE_URL =
-  'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-596cda66f0747475f74108ccc47067de3786961428935f77fc53f37507af856b&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&response_type=code';
+    'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-f52da636dd6edc444bb9854b664906654754e74cb7cc4fe1d448ee6930136d84&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&response_type=code';
 const CONST_BACKEND_URL = 'http://localhost:3000';
 
 export { CONST_AUTHORIZE_URL, CONST_BACKEND_URL };
